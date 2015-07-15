6890c4c7c8e78ee1300f869b7f0f6efa59cc2d72
elvece <lvcifferello@gmail.com>
2015-07-15 13:45:52 -0600

    finished solutions through problem 6, working on bonus still

---
+++ main.js
@@ -0,0 +1,89 @@
+//example
+function test(str) {
+  return "test" + str
+}
+
+console.log(test("ing"))
+
+//problem 1
+function sum(num1,num2){
+  return num1 + num2
+}
+
+console.log(sum(1,2));
+
+//problem 2
+function isEqual(foo, bar) {
+  if (foo === bar){
+  return true
+ }
+  else { return false
+ }
+};
+
+console.log(isEqual("no","yes"));
+
+//problem 3
+
+function discountPercentage(amount, discount) {
+  var totalDiscount= amount * (discount*.01);
+  if (totalDiscount > 100 || totalDiscount < 0) {
+    alert("Discount is absurd!")
+  }   return "$" + totalDiscount;
+}
+console.log(discountPercentage(100, 25));
+
+//problem 4 string to array and then do for loop on array to call first element in array
+function stringCapitalize(string){
+  var intoArray = string.split(" ");
+  for (var i = 0; i < intoArray.length; i++){
+    var j = intoArray[i].charAt(0).toUpperCase();
+    intoArray[i] = j + intoArray[i].substr(1);
+  } return intoArray.join(" ");
+}
+
+console.log(stringCapitalize("lucy victoria cifferello"))
+
+//problem 5
+function evenNumber() {
+  var random = Math.floor(Math.random() * 100);
+  for (var i = 0; i <= random; i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+  }
+}
+console.log(evenNumber());
+
+//problem 6
+function isDivisible (one, two) {
+  if (one % two === 0){
+    return true
+  }
+  else {
+    return false
+  }
+}
+console.log(isDivisible(4,2));
+
+//problem 7
+function oddNumbers() {
+  var random = Math.floor(Math.random() * 100);
+
+  if (random > 40)  {
+    for (var i = 0; i <= random; i++) {
+      if (i % 2 !== 0) {
+      console.log(i);
+      }
+    }
+  }
+
+  if (random < 40) {
+    for (var i = 0; i >= random; i++) {
+      if (i % 2 !== 0) {
+        console.log(i);
+      }
+    }
+  }
+}
+console.log(oddNumbers());
